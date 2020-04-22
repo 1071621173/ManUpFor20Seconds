12129321712adab1e87604cb878d4319b12dca74
jimraynor0 <jimraynor.sko@gmail.com>
2020-04-22 20:02:40 +1000

    Add a player controllable spacecraft

---
+++ Assets/Scripts/InGameControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InGameControl : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        var movement = context.ReadValue<Vector2>();
+        rigidBody.velocity = movement * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
