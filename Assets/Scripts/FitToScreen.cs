13447069f72e0f5bed167dac9d9a251f3cacbae1
jimraynor0 <jimraynor.sko@gmail.com>
2020-04-27 07:59:51 +1000

    Add play area with collider borders

---
+++ Assets/Scripts/FitToScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitToScreen : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      float height = Camera.main.orthographicSize * 2;
+      float width = height * Screen.width / Screen.height;
+
+      Sprite s = spriteRenderer.sprite;
+
+      float unitWidth = s.textureRect.width / s.pixelsPerUnit;
+      float unitHeight = s.textureRect.height / s.pixelsPerUnit;
+
+      spriteRenderer.transform.localScale = new Vector3(width / unitWidth, height / unitHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
