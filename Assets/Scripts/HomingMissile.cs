184c5cc649a4b3fde9f65c1e9efdb0411844abd7
weishui <kevin.feng@hotmail.com>
2020-05-11 16:12:25 +0800

    ReUpload

---
+++ Assets/Scripts/HomingMissile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingMissile : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+    private Transform target;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - rb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+        rb.velocity = transform.up * speed;
+    }
+}
