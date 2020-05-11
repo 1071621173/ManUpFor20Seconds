6e214b5b8f50af5585a5cc6280f57f20687c35ec
weishui <kevin.feng@hotmail.com>
2020-05-11 16:00:06 +0800

    BugFix2

---
+++ Assets/Scripts/ScoreTextController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class ScoreTextController : MonoBehaviour
+{
+    private Text text;
+    private float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        Profile.score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Profile.score = Mathf.FloorToInt(timer * 100);
+        text.text = "Score: " + Profile.score;
+    }
+}
